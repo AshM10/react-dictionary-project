487f00c36ea3bf82f6006f76678be89562f07632
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-05-11 11:02:27 -0500

    Search engine setup.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid rounded" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid rounded" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Ash Moreno</small>
+        </footer>
+      </div>
     </div>
   );
 }
