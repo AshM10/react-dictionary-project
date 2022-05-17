2751217928cc2d4a38e0ad954686293e6c5758a2
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-05-17 14:14:22 -0500

    Added color styling.

--- src/App.js
+++ src/App.js
@@ -6,32 +6,34 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid rounded" alt="logo" />
-        </header>
-        <main>
-          <Dictionary defaultKeyword="donut" />
-        </main>
-        <footer className="App-footer">
-          <small>
-            This project was coded by{" "}
-            <a
-              href="https://www.linkedin.com/in/asthermoreno10/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Ash Moreno
-            </a>
-            , and is {""}
-            <a
-              href="https://github.com/AshM10/react-dictionary-project"
-              target="_blank"
-              rel="noreferrer"
-            >
-              open-sourced on Github.
-            </a>
-          </small>
-        </footer>
+        <div className="content-wrapper">
+          <header className="App-header">
+            <img src={logo} className="App-logo img-fluid rounded" alt="logo" />
+          </header>
+          <main>
+            <Dictionary defaultKeyword="donut" />
+          </main>
+          <footer className="App-footer">
+            <small>
+              This project was coded by{" "}
+              <a
+                href="https://www.linkedin.com/in/asthermoreno10/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Ash Moreno
+              </a>
+              , and is {""}
+              <a
+                href="https://github.com/AshM10/react-dictionary-project"
+                target="_blank"
+                rel="noreferrer"
+              >
+                open-sourced on Github.
+              </a>
+            </small>
+          </footer>
+        </div>
       </div>
     </div>
   );
